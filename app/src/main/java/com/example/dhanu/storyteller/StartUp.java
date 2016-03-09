181d44a76de09ec10b566d9720ee2727a133a8de
Asbar <asbar.create@gmail.com>
2016-03-10 03:15:36 +0530

    hello

--- app/src/main/java/com/example/dhanu/storyteller/StartUp.java
+++ app/src/main/java/com/example/dhanu/storyteller/StartUp.java
@@ -1,13 +1,31 @@
 package com.example.dhanu.storyteller;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartUp extends AppCompatActivity {
 
+    Button b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b=(Button)findViewById(R.id.button);
+
+        b.setOnClickListener(
+                new View.OnClickListener(){
+
+                    @Override
+                    public void onClick(View v) {
+                        Intent i=new Intent(getApplicationContext(),Creating_Story.class);
+                        startActivity(i);
+                    }
+                }
+        );
     }
+
+
 }
